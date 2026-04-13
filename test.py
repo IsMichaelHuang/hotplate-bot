495c2370674aeeaaffabd938a1d4c1786710ca79
IsMichaelHuang <michael.micah.huang@gmail.com>
2026-04-13 11:36:21 -0700

    Build test script

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
+from dotenv import load_dotenv
+
+import os
 import nodriver as uc
 
-async def main():
+async def main(**kargs):
+    url = kargs["website"]
+
     browser = await uc.start()
-    page = await browser.get("https://www.hotplate.com/bakeryinabasket")
+    page = await browser.get(url)
     await page.sleep(3)
     await page.save_screenshot("test.png")
     await browser.stop()
 
 if __name__ == "__main__":
-    uc.loop().run_until_complete(main())
+    load_dotenv()
+    website = os.getenv("WEBSITE_URL")
+    uc.loop().run_until_complete(main(website=website))
 
